0b6d2037e603de2adb55cdbe9148aa7ee81fdf64
Seba <sebastianmatamala462@gmail.com>
2024-12-12 22:56:58 -0300

    clase de introduccion

---
+++ intro/script.js
@@ -0,0 +1,43 @@
+"tipos de datos js"
+
+undefined
+null
+"boolean verdadero o falso"
+"number numeros"
+"string cadena de texto"
+"object const = { a: 1, b: 2 }"
+"symbol simbolo"
+"bigint numeros grandes"
+
+const a = '1'; // es un string
+console.log(a === 1); // false porque no son del mismo tipo
+
+
+"= para asignar un valor a una variable"
+"== para comparar dos valores"
+"=== para comparar dos valores y que sean del mismo tipo de dato"
+"> mnayor que"
+">= mayor o igual"
+"< menor que"
+"<= menor o igual"
+"!== diferente"
+
+
+"operadores logicos"
+"&& and para que dos condiciones sean verdaderas"
+"|| or para que una de las condiciones sean verdaderas"
+"! not para que una condicion sea falsa"
+
+
+"operadores de incremento y decremento"
+"++"
+"--"
+
+
+"operadores aritmeticos"
+"+"
+"-"
+"*"
+"/"
+"%"
+
